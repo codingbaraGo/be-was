0a7cc2001a7b324511f27f34e17bf43865b36ed4
codingbaraGo <hyunchang52@gmail.com>
2026-01-03 00:16:06 +0900

    feat(exception): Custom Exception 클래스 정의 - 비지니스 로직 내 발생하는 Exception인 ServiceException 클래스 정의 - 비지니스 로직 외 Error를 던지기 위한 Exception 클래스인 ErrorException 클래스 정의

---
+++ src/main/java/webserver/exception/ErrorException.java
@@ -0,0 +1,26 @@
+package webserver.exception;
+
+public class ErrorException extends RuntimeException {
+    private final ErrorCode errorCode;
+    private final Throwable throwable;
+
+    public ErrorException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.INTERNAL_ERROR;
+        this.throwable = null;
+    }
+
+    public ErrorException(String message, Throwable t) {
+        super(message);
+        this.errorCode = ErrorCode.INTERNAL_ERROR;
+        this.throwable = t;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+}

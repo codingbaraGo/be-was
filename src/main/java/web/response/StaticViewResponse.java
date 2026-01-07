bd4a3903a0f707f887fd823b56f18e38beed3460
codingbaraGo <hyunchang52@gmail.com>
2026-01-07 20:45:20 +0900

    refactor(web-renderer): StaticViewResponse의 멤버 path에 final 제어자 추가

--- src/main/java/web/response/StaticViewResponse.java
+++ src/main/java/web/response/StaticViewResponse.java
@@ -3,36 +3,22 @@ package web.response;
 import http.HttpStatus;
 
 public class StaticViewResponse extends HandlerResponse {
-    private String path;
+    private final String path;
 
-    public StaticViewResponse(String path) {
-        super(HttpStatus.OK);
+    public StaticViewResponse(HttpStatus status, String path){
+        super(status);
         this.path = path;
     }
 
-    public StaticViewResponse(HttpStatus status){
-        super(status);
-        this.path = "";
-    }
-
-    public static StaticViewResponse of(HttpStatus status){
-        return new StaticViewResponse(status);
-    }
-    public static StaticViewResponse of(HttpStatus status, String viewPath){
-        StaticViewResponse response = new StaticViewResponse(status);
-        response.setPath(viewPath);
-        return response;
+    public static StaticViewResponse of(HttpStatus status, String path){
+        return new StaticViewResponse(status, path);
     }
 
     public static StaticViewResponse of (String path){
-        return new StaticViewResponse(path);
+        return new StaticViewResponse(HttpStatus.OK, path);
     }
 
     public String getPath() {
         return path;
-    }
-
-    public void setPath(String path){
-        this.path = path;
     }
 }

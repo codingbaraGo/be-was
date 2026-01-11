422f02df125fa4139c384cf8f8f26a3f9c1d4417
codingbaraGo <hyunchang52@gmail.com>
2026-01-11 14:57:29 +0900

    refactor(config-di): AppConfig에서 함수 호출시 싱글톤 보장 - SingletonContainer 추상 클래스 정의 - 제네릭을 사용한 싱글톤 컨테이너 구축

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -11,127 +11,202 @@ import http.request.HttpRequestConverter;
 import http.request.InputStreamHttpRequestConverter;
 import http.response.HttpResponseBufferedStreamConverter;
 import http.response.HttpResponseConverter;
-import web.dispatch.argument.ArgumentResolver;
 import web.dispatch.Dispatcher;
 import web.dispatch.HandlerAdapter;
 import web.dispatch.adapter.DefaultHandlerAdapter;
 import web.dispatch.adapter.SingleArgHandlerAdapter;
+import web.dispatch.argument.ArgumentResolver;
 import web.dispatch.argument.resolver.HttpRequestResolver;
 import web.dispatch.argument.resolver.QueryParamsResolver;
 import web.handler.StaticContentHandler;
 import web.handler.WebHandler;
-import web.renderer.StaticViewRenderer;
 import web.renderer.HttpResponseRenderer;
+import web.renderer.StaticViewRenderer;
 
 import java.util.List;
 
-public class AppConfig {
-    //Http
-    public HttpRequestConverter httpRequestConverter(){
+public class AppConfig extends SingletonContainer {
+
+    /**
+     * ===== Http =====
+     */
+    public HttpRequestConverter httpRequestConverter() {
         return inputStreamHttpRequestConverter();
     }
-    public HttpResponseConverter httpResponseConverter(){
+
+    public HttpResponseConverter httpResponseConverter() {
         return httpResponseBufferedStreamConverter();
     }
 
-    public BufferedReaderHttpRequestConverter httpBufferedReaderRequestConverter(){
-        return new BufferedReaderHttpRequestConverter();
-    }
-
-    public HttpResponseBufferedStreamConverter httpResponseBufferedStreamConverter(){
-        return new HttpResponseBufferedStreamConverter();
-    }
-    public InputStreamHttpRequestConverter inputStreamHttpRequestConverter(){
-        return new InputStreamHttpRequestConverter();
-    }
-
-
-    //Web
-    public Dispatcher dispatcher(){
-        return new Dispatcher(
-                webHandlerList(),
-                handlerAdapterList(),
-                webHandlerResponseHandlerList()
+    public BufferedReaderHttpRequestConverter httpBufferedReaderRequestConverter() {
+        return getOrCreate(
+                "httpBufferedReaderRequestConverter",
+                BufferedReaderHttpRequestConverter::new
         );
     }
 
-    private List<WebHandler> webHandlerList(){
-        return List.of(
-                staticContentHandler(),
-                registerWithGet(),
-                registerWithPost()
-        );
-    }
-    private RegisterWithGet registerWithGet(){
-        return new RegisterWithGet();
-    }
-    private RegisterWithPost registerWithPost(){
-        return new RegisterWithPost();
-    }
-
-    private List<HttpResponseRenderer> webHandlerResponseHandlerList(){
-        return List.of(
-                staticViewResponseHandler()
-        );
-    }
-    private StaticContentHandler staticContentHandler(){
-        return new StaticContentHandler();
-    }
-    private StaticViewRenderer staticViewResponseHandler(){
-        return new StaticViewRenderer();
-    }
-
-
-    //Adapter
-    public List<HandlerAdapter> handlerAdapterList(){
-        return List.of(
-                singleArgHandlerAdapter(),
-                defaultHandlerAdapter()
+    public HttpResponseBufferedStreamConverter httpResponseBufferedStreamConverter() {
+        return getOrCreate(
+                "httpResponseBufferedStreamConverter",
+                HttpResponseBufferedStreamConverter::new
         );
     }
 
-    private SingleArgHandlerAdapter singleArgHandlerAdapter(){
-        return new SingleArgHandlerAdapter(
-                argumentResolverList()
-        );
-    }
-    private DefaultHandlerAdapter defaultHandlerAdapter(){
-        return new DefaultHandlerAdapter();
-    }
-
-    //Resolver
-    public List<ArgumentResolver<?>> argumentResolverList(){
-        return List.of(
-                httpRequestResolver(),
-                queryParamsResolver()
+    public InputStreamHttpRequestConverter inputStreamHttpRequestConverter() {
+        return getOrCreate(
+                "inputStreamHttpRequestConverter",
+                InputStreamHttpRequestConverter::new
         );
     }
 
-    private HttpRequestResolver httpRequestResolver(){
-        return new HttpRequestResolver();
-    }
-    private QueryParamsResolver queryParamsResolver(){
-        return new QueryParamsResolver();
-    }
-
-    //Exception
-    public ExceptionHandlerMapping exceptionHandlerMapping(){
-        return new ExceptionHandlerMapping(
-                List.of(
-                        serviceExceptionHandler(),
-                        errorExceptionHandler(),
-                        unhandledErrorHandler()
+    /**
+     * ===== Web =====
+     */
+    public Dispatcher dispatcher() {
+        return getOrCreate(
+                "dispatcher",
+                () -> new Dispatcher(
+                        webHandlerList(),
+                        handlerAdapterList(),
+                        webHandlerResponseHandlerList()
                 )
         );
     }
 
-    private ServiceExceptionHandler serviceExceptionHandler(){
-        return new ServiceExceptionHandler();
+    public List<WebHandler> webHandlerList() {
+        return getOrCreate(
+                "webHandlerList",
+                () -> List.of(
+                        staticContentHandler(),
+                        registerWithGet(),
+                        registerWithPost()
+                )
+        );
     }
-    private UnhandledErrorHandler unhandledErrorHandler(){
-        return new UnhandledErrorHandler();
+
+    public RegisterWithGet registerWithGet() {
+        return getOrCreate(
+                "registerWithGet",
+                RegisterWithGet::new
+        );
     }
-    private ErrorExceptionHandler errorExceptionHandler(){
-        return new ErrorExceptionHandler();
+
+    public RegisterWithPost registerWithPost() {
+        return getOrCreate(
+                "registerWithPost",
+                RegisterWithPost::new
+        );
+    }
+
+    public List<HttpResponseRenderer> webHandlerResponseHandlerList() {
+        return getOrCreate(
+                "webHandlerResponseHandlerList",
+                () -> List.of(
+                        staticViewResponseHandler()
+                )
+        );
+    }
+
+    public StaticContentHandler staticContentHandler() {
+        return getOrCreate(
+                "staticContentHandler",
+                StaticContentHandler::new
+        );
+    }
+
+    public StaticViewRenderer staticViewResponseHandler() {
+        return getOrCreate(
+                "staticViewResponseHandler",
+                StaticViewRenderer::new
+        );
+    }
+
+    // ===== Adapter =====
+    public List<HandlerAdapter> handlerAdapterList() {
+        return getOrCreate(
+                "handlerAdapterList",
+                () -> List.of(
+                        singleArgHandlerAdapter(),
+                        defaultHandlerAdapter()
+                )
+        );
+    }
+
+    public SingleArgHandlerAdapter singleArgHandlerAdapter() {
+        return getOrCreate(
+                "singleArgHandlerAdapter",
+                () -> new SingleArgHandlerAdapter(
+                        argumentResolverList()
+                )
+        );
+    }
+
+    public DefaultHandlerAdapter defaultHandlerAdapter() {
+        return getOrCreate(
+                "defaultHandlerAdapter",
+                DefaultHandlerAdapter::new
+        );
+    }
+
+    // ===== Resolver =====
+    public List<ArgumentResolver<?>> argumentResolverList() {
+        return getOrCreate(
+                "argumentResolverList",
+                () -> List.of(
+                        httpRequestResolver(),
+                        queryParamsResolver()
+                )
+        );
+    }
+
+    public HttpRequestResolver httpRequestResolver() {
+        return getOrCreate(
+                "httpRequestResolver",
+                HttpRequestResolver::new
+        );
+    }
+
+    public QueryParamsResolver queryParamsResolver() {
+        return getOrCreate(
+                "queryParamsResolver",
+                QueryParamsResolver::new
+        );
+    }
+
+    /**
+     * ===== Exception =====
+     */
+    public ExceptionHandlerMapping exceptionHandlerMapping() {
+        return getOrCreate(
+                "exceptionHandlerMapping",
+                () -> new ExceptionHandlerMapping(
+                        List.of(
+                                serviceExceptionHandler(),
+                                errorExceptionHandler(),
+                                unhandledErrorHandler()
+                        )
+                )
+        );
+    }
+
+    public ServiceExceptionHandler serviceExceptionHandler() {
+        return getOrCreate(
+                "serviceExceptionHandler",
+                ServiceExceptionHandler::new
+        );
+    }
+
+    public UnhandledErrorHandler unhandledErrorHandler() {
+        return getOrCreate(
+                "unhandledErrorHandler",
+                UnhandledErrorHandler::new
+        );
+    }
+
+    public ErrorExceptionHandler errorExceptionHandler() {
+        return getOrCreate(
+                "errorExceptionHandler",
+                ErrorExceptionHandler::new
+        );
     }
 }

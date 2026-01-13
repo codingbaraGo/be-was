44f875b625b275f5a798e7a98b9a1169afd435f9
codingbaraGo <hyunchang52@gmail.com>
2026-01-13 17:09:23 +0900

    enhance(auth): AuthenticationInfo에 Attribute 추가 - AuthenticationInfo에 Attribute를 설정하고 조회할 수 있는 기능 추가

--- src/main/java/web/filter/authentication/UserAuthentication.java
+++ src/main/java/web/filter/authentication/UserAuthentication.java
@@ -1,18 +1,32 @@
 package web.filter.authentication;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class UserAuthentication implements AuthenticationInfo {
     private final Long userId;
     private final UserRole userRole;
+    private final Map<String, Object> attributes;
 
     private UserAuthentication(Long userId, UserRole userRole) {
         this.userId = userId;
         this.userRole = userRole;
+        this.attributes = new HashMap<>();
     }
 
     public static UserAuthentication of(Long userId, UserRole userRole){
         return new UserAuthentication(userId, userRole);
+    }
+
+    @Override
+    public void addAttribute(String key, Object value) {
+        attributes.put(key, value);
+    }
+
+    @Override
+    public Optional<Object> getAttribute(String key) {
+        return Optional.of(attributes.get(key));
     }
 
     @Override
